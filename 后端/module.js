680257da1604f41ff51b5b15b76a82d6ddef2559
WGXWGX <wgx15145076862@sina.com>
2016-08-11 20:27:34 +0800

    整理

--- 后端/module.js
+++ 后端/module.js
@@ -2,10 +2,6 @@ var hello=require('./getMoudle');//1
 var hello = new hello();
 hello.setName();
 hello.getName();
-
-
-
-
 var hello=require('./getMoudle').hello;//2
 var hello = new hello();
 hello.setName();
