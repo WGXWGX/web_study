73a77edcc6d4f79173f00cdd9b1cba7478c339f8
WGXWGX <wgx15145076862@sina.com>
2016-08-24 17:47:09 +0800

    test

---
+++ test/01-test.php
@@ -0,0 +1,21 @@
+<style>
+	table{
+		border:1px solid #050;
+	}
+	th,td{
+		height: 30px;
+		text-align: center;
+	}
+	td{
+		width: 30px;
+	}
+	.font{
+    	color: white;
+    	background: blue;
+	}
+</style>
+<?php
+	include "rili.php";
+	$rili=new rili();
+	$rili->out();
+?>
