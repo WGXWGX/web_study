b0bd41f1eef320b84ff17ab51ae986262ae8917f
WGXWGX <wgx15145076862@sina.com>
2016-09-05 21:59:45 +0800

    Update nav.js

--- 唯品会/nav.js
+++ 唯品会/nav.js
@@ -18,4 +18,46 @@ $(function(){
 		$('#blank').hide();
 		$('#moreaddress').hide();
 	});
-}
+	$addressName.not('.word').on('click',function(){
+		var $value = $(this).html();
+		$('#defaultaddress').html("<div>" + $value + "</div>");
+	});
+	$('#topli2').hover(function(){
+		$('#topli2 #phone1').hide();
+		$('#topli2 #pnone').show();
+		$('#blankphone').show();
+		$('#wrapcode').show();
+		$(this).css({
+			background: '#fff',
+			color: '#f10582'
+		});
+	},function(){
+		$('#topli2 #phone').hide();
+		$('#topli2 #phone1').show();
+		$('#blankphone').hide();
+		$('#wrapcode').hide();
+		$(this).css({
+			background: '#f5f5f5',
+			color: '#777'
+		});
+	});
+	$('#mainnav li').on('click',function(){
+		$(this).addClass('selected2').siblings().removeClass('selected2');
+		$('#navlast').removeClass('selected2');
+	});
+	$('#navlast').on('click',function(){
+		$(this).addClass('selected2');
+		$('#mainnav li').removeClass('selected2');
+	});
+	$('#imghint li').hover(function(){
+		$(this).addClass('selected-imghint').siblings().removeClass('selected-imghint');
+		$('#imgtwo').animate({
+			opacity: 1
+		},500);
+		$('#imgone').animate({
+			opacity: 0
+		},500);
+	},function(){});
+
+});
+
