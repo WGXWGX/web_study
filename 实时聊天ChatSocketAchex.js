7029dd003e340afcba17c1d987d872c23b3a69b7
WGXWGX <wgx15145076862@sina.com>
2016-12-10 20:27:40 +0800

    Update 实时聊天ChatSocketAchex.js

--- 实时聊天ChatSocketAchex.js
+++ 实时聊天ChatSocketAchex.js
@@ -96,7 +96,56 @@ $.fn.extend({ChatSocket: function(opciones) {
           }
            
          
-         
+          function CrearEntrada(){
+          $('#'+idChat).append('<div id="'+idDialogo+'" class="'+classChat+'" id="InputNombre"><div class="panel-footer" style="margin-top:100px;"><div class="input-group"><input id="'+lblTxtEntrar+'" type="text" class="form-control input-sm" placeholder="'+lblEntradaNombre+'"><span class="input-group-btn"><button id="'+btnEntrar+'" class="btn btn-success btn-sm" >'+lblBtnEntrar+'</button></span></div></div></div>');
+         $('#'+idOnline).append(' <div class="panel panel-'+panelColor+'"><div class="panel-heading"><span class="glyphicon glyphicon-user"></span> '+lblUsuariosOnline+'</div><div class="panel-body"><ul class="list-group" id="listaOnline"></ul></div><div class="panel-footer"><div class="input-group"><div><a href="http://develoteca.com">by develoteca.com</a></div></span></div></div></div>');
+              $("#"+lblTxtEntrar).keyup(function (e) {if (e.keyCode == 13) { iniciarChat(); }});
+              $("#"+btnEntrar).click(function(){
+              iniciarChat();
+              });
+        }
+          function CrearChat(){
+             $('#'+idChat ).append( '<div class="'+classChat+'"><div class="panel panel-'+panelColor+'"><div class="panel-heading"><span class="glyphicon glyphicon-comment"></span>'+lblTitulChat+" : "+Nombre+'<div class="btn-group pull-right"><button type="button" onclick="alert(\''+textoAyuda+'\')" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-chevron-down"></span></button></div></div><div class="panel-body"><ul class="chatpluginchat"></ul></div><div class="panel-footer"><div class="input-group"><input id="'+lblTxtEnviar+'" type="text" class="form-control input-sm" placeholder="'+lblCampoEntrada+'" /><span class="input-group-btn"><button  class="btn btn-warning btn-sm" id="'+btnEnviar+'">'+lblEnviar+'</button></span></div></div></div></div><li class="left clearfix itemtemplate" style="display:none"><span class="chat-img pull-left"><img src="'+urlImg+'" alt="User Avatar" class="img-circle" id="Foto"/></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font" id="Nombre">Nombre</strong><small class="pull-right text-muted"><span class="glyphicon glyphicon-asterisk"></span><span id="Tiempo">12 mins ago</span></small></div> <p id="Contenido">Contenido</p></div></li>');
+              
+    $("#"+lblTxtEnviar).keyup(function (e) {if (e.keyCode == 13) { EnviarMensaje();}});
+    $("#"+btnEnviar).click(function () {EnviarMensaje();});
+              
+        }
+      
+        function EnviarMensaje(){
+           ws.send('{"to":"'+Room+'","Nombre":"'+Nombre+'","Contenido":"'+$('#'+lblTxtEnviar).val()+'"}');
+        $("#"+lblTxtEnviar).val('');
+          
+        };
+        function UsuarioOnline(){
+           ws.send('{"to":"'+Room+'","Nombre":"'+Nombre+'"}');
+        }
+        function AgregarItem(Obj){
+            
+            if((Obj.Contenido!=null)&&(Obj.Nombre!=null)){
+                
+            $( ".itemtemplate" ).clone().appendTo( ".chatpluginchat" );
+            $('.chatpluginchat .itemtemplate').show(10);
+            $('.chatpluginchat .itemtemplate #Nombre').html(Obj.Nombre);
+            $('.chatpluginchat .itemtemplate #Contenido').html(Obj.Contenido);
+             
+             var formattedDate = new Date();
+             var d = formattedDate.getUTCDate();
+             var m =  formattedDate.getMonth();
+             var y = formattedDate.getFullYear();
+             var h= formattedDate.getHours();
+             var min= formattedDate.getMinutes();
+            
+            Fecha=d+"/"+m+"/"+y+" "+h+":"+min;
+            
+            $('.chatpluginchat .itemtemplate #Tiempo').html(Fecha);
+            $('.chatpluginchat .itemtemplate').removeClass("itemtemplate");
+            }
+        }
+           function getOnline() {
+                setInterval(UsuarioOnline, 3000);
+            }
+           
     
            
          CrearEntrada();
